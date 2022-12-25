12686f4bc69ab4eafa63ebf43cc20e86cd5179fe
taltolo <taltolo993@gmail.com>
2022-12-25 17:45:57 +0200

    edit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect('mongodb://127.0.0.1/ArcPlaner', {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedtopology: true,
   })
