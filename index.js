7e2bcf4706825e9f6127c745dbbf3dea032eafa7
talt <taltolo993@gmail.com>
2022-12-29 22:58:59 +0200

    edit url

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/network/db', networkRouter);
+app.use('/', networkRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
