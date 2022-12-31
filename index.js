643a18be21939a84e2c7175586267c4ab695aa70
talt <taltolo993@gmail.com>
2022-12-31 16:18:46 +0200

    edit url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(process.env.MONGODB_URI);
 console.log(typeof process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb+srv://talt:TB7570!!@cluster0.hwv4qbr.mongodb.net/?retryWrites=true&w=majority", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedtopology: true,
   })
