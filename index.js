a66e70d5bbc82d87e73b9d5dd54fb39aa0e5bff6
talt <taltolo993@gmail.com>
2022-12-31 16:43:52 +0200

    edit url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(process.env.MONGODB_URI);
 console.log(typeof process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect("mongodb+srv://"+process.env.USER_NANE_DB+":"+process.env.PASSWORD_DB+"@cluster0.hwv4qbr.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedtopology: true,
   })
